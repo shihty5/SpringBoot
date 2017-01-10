6ba4e54fe7f5454c309b6a80978ee073b96c6ce2
Peter <shihty5@163.com>
2017-01-10 23:34:23 +0800

    Controller
    @RestController = @Controller + @ResponseBody
    @RequestMapping
    @PostMapping
    @PathVariable
    @RequestParam

--- src/main/java/com/peter/HelloController.java
+++ src/main/java/com/peter/HelloController.java
@@ -1,19 +1,34 @@
 package com.peter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-	
+
 	@Autowired
 	private GirlProperties girl;
+
+	@RequestMapping(value = "/say/{id}", method = RequestMethod.GET)
+	public String sayRest(@PathVariable("id") Integer id) {
+		// return girl.getCupSize() + id;
+		return "id : " + id;
+	}
+
+	@RequestMapping(value = "/say", method = RequestMethod.GET)
+	public String sayParam(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+		// return girl.getCupSize() + id;
+		return "id : " + id;
+	}
 	
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String say(){
-		return girl.getCupSize();
-		
+	@PostMapping(value ="/post")
+	public String sayPost(@PathVariable("post") String post){
+		return post + girl.getCupSize();
 	}
 }
