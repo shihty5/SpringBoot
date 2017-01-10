7a4ed421054f307d0d5b701fd19c973ad0e526e2
1 <1@DESKTOP-EQAI2TQ.lan>
2017-01-10 22:22:49 +0800

    first try

---
+++ src/test/java/com/peter/HelloSpringBootTests.java
@@ -0,0 +1,19 @@
+package com.peter;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloSpringBootTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
