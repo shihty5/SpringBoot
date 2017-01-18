7f0370af3d8934d9c87e939584fdf003fbee4869
Peter <shihty5@163.com>
2017-01-19 00:40:43 +0800

    HttpRequest for Sonar Web API

--- src/main/java/com/sonar/SonarAnalyzer.java
+++ src/main/java/com/sonar/SonarAnalyzer.java
@@ -1,10 +1,11 @@
 package com.sonar;
 
+import java.io.IOException;
+
 public class SonarAnalyzer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		SonarUtil.getReq("http://localhost:9090/api/resources");
 	}
 
 }
